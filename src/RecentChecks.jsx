d7f2d65f7618fa6fbb0b5ee82fd2996358412076
pburglin <pedro.burglin@gmail.com>
2025-02-02 11:54:24 -0700

    Initial commit

--- src/RecentChecks.jsx
+++ src/RecentChecks.jsx
@@ -7,7 +7,10 @@ const RecentChecks = ({ checks }) => {
       <ul>
         {checks.map((check, index) => (
           <li key={index}>
-            {check.url}: {check.status.up ? 'Up' : 'Down'}
+            {check.url}: {check.status.up ? 'Up' : 'Down'} (HTTP {check.status})
+            <span style={{ fontSize: '0.8rem', marginLeft: '0.5rem', color: '#777' }}>
+              {new Date(check.timestamp).toLocaleTimeString()}
+            </span>
           </li>
         ))}
       </ul>
