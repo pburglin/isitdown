24da4c5bc819513b41632286eb865b42e3d9080f
Pedro Burglin <pedro.burglin@gmail.com>
2025-02-02 20:33:18 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => (
     <div className="app-container">
       <h1 style={{ textAlign: 'center' }}>URL Status Checker</h1>
       <p style={{ textAlign: 'center', margin: '1rem 0' }}>
-        Did you ever see a website down and wondered if the problem is on their side or with your internet connection? Use this tool to wonder no more. Just enter the website's address to perform a fresh site status test in real time.
+        Did you ever see a website down and wondered if the problem is on their side or with your internet connection? Use this tool and wonder no more. Just enter the website's address to perform a fresh site status test in real time.
       </p>
       <URLChecker />
     </div>
