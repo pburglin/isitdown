43fedf188be6f5dbfe0bf56abaa5ac2640bebeb5
Pedro Burglin <pedro.burglin@gmail.com>
2025-02-02 22:30:11 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ErrorBoundary from './ErrorBoundary'
 const App = () => (
   <ErrorBoundary>
     <div className="app-container">
-      <h1 style={{ textAlign: 'center' }}>URL Status Checker</h1>
+      <h1 style={{ textAlign: 'center' }}>Site Checker</h1>
       <p style={{ textAlign: 'center', margin: '1rem 0' }}>
         Did you ever see a website down and wondered if the problem is on their side or with your internet connection? Use this tool and wonder no more. Just enter the website's address to perform a fresh site status test in real time.
       </p>
